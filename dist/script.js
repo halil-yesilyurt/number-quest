ae0d60c5cd2e4a38082b70c6d2cb63a164a65b6d
Halil Y. <haliilyesilyurt@gmail.com>
2024-06-23 18:00:38 +0300

    update variables and number create function

--- dist/script.js
+++ dist/script.js
@@ -1,29 +1,38 @@
 const number = document.querySelector('.number');
-const scoreEl = document.querySelector('.score');
-const highScore = document.querySelector('.highscore');
+let scoreEl = document.querySelector('.score');
+const highScoreEl = document.querySelector('.highscore');
 const btnCheck = document.querySelector('.btn-check');
 const btnAgain = document.querySelector('.btn-again');
 let message = document.querySelector('.message');
-let luckyNumber = Math.trunc(Math.random() * 30 + 1);
+let luckyNumber = createNumber();
 let score = 30;
+let highScore = Number(highScoreEl.textContent);
 
 function showMessage(text, color = '#edede9') {
   message.textContent = text;
   message.style.color = color;
 }
-console.log(luckyNumber);
+
+function createNumber() {
+  return Math.trunc(Math.random() * 30 + 1);
+}
+
 function checkNumber() {
   const guess = Number(document.querySelector('.guess').value);
-  // if there is no input
   if (!guess) {
     showMessage('Please enter a number!', '#982222');
   } else if (guess === luckyNumber) {
     showMessage('Number is correct!', '#13006e');
-    highScore.textContent = score;
     number.textContent = luckyNumber;
     number.style.width = '13rem';
     document.querySelector('body').style.backgroundImage = 'linear-gradient(to right, #00b09b, #96c93d)';
-  } else if (guess !== luckyNumber) {
+    btnCheck.disabled = true;
+    document.querySelector('.guess').readOnly = true;
+    if (highScore < score) {
+      highScore = score;
+      highScoreEl.textContent = highScore;
+    }
+  } else {
     if (score > 1) {
       showMessage(guess < luckyNumber ? 'Too low' : 'Too high');
       score--;
